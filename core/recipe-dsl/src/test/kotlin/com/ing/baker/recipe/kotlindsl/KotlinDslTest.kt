7f8d32d670b4345e923ff1d990f81c8eee3a4036
Willem Veelenturf <willem.veelenturf@gmail.com>
2023-01-25 10:28:45 +0100

    Add test

---
+++ core/recipe-dsl/src/test/kotlin/com/ing/baker/recipe/kotlindsl/KotlinDslTest.kt
@@ -0,0 +1,12 @@
+package com.ing.baker.recipe.kotlindsl
+
+import org.junit.Test
+
+class KotlinDslTest {
+
+    @Test
+    fun `should be able to have object as ingredient` () {
+        println("123123")
+    }
+
+}
